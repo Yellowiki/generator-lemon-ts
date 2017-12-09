66090bb17810f70716aea8e69a2a5049ea800778
GloriousYellow <yellowglorious@gmail.com>
2017-12-09 17:35:55 -0500

    refactor: move generated package.json into it's own file

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,7 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const yosay = require('yosay');
+const deepAssign = require('deep-assign');
 
 module.exports = class extends Generator {
   prompting() {
@@ -53,65 +54,14 @@ module.exports = class extends Generator {
 
   async writing() {
     await fs.copy(this.templatePath(), this.destinationPath());
-    const pkg = {
+    const pkg = deepAssign(await fs.readJSON(this.templatePath('package.json')), {
       name: this.props.name,
-      version: '0.0.0-development',
       description: this.props.name,
-      license: 'MIT',
-      files: ['dist'],
-      main: 'dist/index.js',
-      typings: 'dist/index.d.ts',
       repository: {
         type: 'git',
         url: this.props.repository
-      },
-      scripts: {
-        clean: 'rimraf dist && rimraf coverage',
-        commitmsg: 'commitlint -e $GIT_PARAMS',
-        format: 'prettier *.json --write && yarn lint --format',
-        lint: 'tslint --project tsconfig.json --format codeFrame',
-        prepare: 'yarn build && yarn test',
-        pretest: 'yarn lint',
-        prebuild: 'yarn clean && yarn format',
-        precommit: 'yarn prepare',
-        build: 'tsc --pretty',
-        test: 'jest --coverage',
-        watch: 'yarn build --watch',
-        'watch:test': 'jest --watch'
-      },
-      devDependencies: {
-        '@commitlint/cli': '^5.2.0',
-        '@commitlint/config-conventional': '^5.1.3',
-        '@types/jest': '^21.1.8',
-        husky: '^0.14.3',
-        jest: '^21.2.1',
-        'jest-environment-node-debug': '^2.0.0',
-        prettier: '^1.5.2',
-        rimraf: '^2.0.0',
-        'semantic-release': '^8.2.0',
-        'ts-jest': '^21.2.3',
-        'ts-node': '^3.2.0',
-        tslint: '^5.0.0',
-        'tslint-config-lemon': '^1.1.7',
-        typescript: '^2.3.0'
-      },
-      dependencies: {
-        '@types/node': '>=8.0.0'
-      },
-      engines: {
-        node: '>=8.0.0'
-      },
-      jest: {
-        transform: {
-          '.(ts)': 'ts-jest'
-        },
-        testRegex: '/__tests__/.*\\.test\\.ts$',
-        moduleFileExtensions: ['ts', 'js', 'json'],
-        testEnvironment: 'node',
-        mapCoverage: true
-      },
-      precommit: 'prepare'
-    };
+      }
+    });
     if (this.props.type === 'cli') {
       pkg.bin = 'dist/index.js';
       pkg.scripts.build = 'tsc --pretty && chmod +x dist/index.js';
